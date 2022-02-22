457e0fa04a19f85bbb95a86e772395af0e698c1c
mellkior <mmandych@firstprinciplesdevelopment.com>
2022-02-22 16:31:10 -0500

    customized migration

--- priv/repo/migrations/20220222212529_create_players.exs
+++ priv/repo/migrations/20220222212529_create_players.exs
@@ -5,11 +5,13 @@ defmodule DictionaryGame.Repo.Migrations.CreatePlayers do
     create table(:players) do
       add :name, :string
       add :is_host, :boolean, default: false, null: false
-      add :room_id, references(:rooms, on_delete: :nothing)
+      add :room_id, references(:rooms, on_delete: :delete_all)
 
       timestamps()
     end
 
     create index(:players, [:room_id])
+    # player names inside a room must be unique
+    create unique_index(:players, [:id, :room_id])
   end
 end
