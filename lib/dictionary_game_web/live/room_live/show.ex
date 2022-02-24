67b54a91e34cd26e534f1cdf72f02b519c7d42a0
mellkior <mmandych@firstprinciplesdevelopment.com>
2022-02-24 11:36:15 -0500

    added player create form on room show

--- lib/dictionary_game_web/live/room_live/show.ex
+++ lib/dictionary_game_web/live/room_live/show.ex
@@ -2,19 +2,20 @@ defmodule DictionaryGameWeb.RoomLive.Show do
   use DictionaryGameWeb, :live_view
 
   alias DictionaryGame.Game
+  alias DictionaryGame.Game.Player
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, :player, %Player{})}
   end
 
   @impl true
   def handle_params(%{"room_code" => room_code}, _, socket) do
     {:noreply,
      socket
-     |> assign(:page_title, page_title(socket.assigns.live_action))
+     |> assign(:page_title, page_title(socket.assigns.live_action, room_code))
      |> assign(:room, Game.get_room_by_room_code!(room_code))}
   end
 
-  defp page_title(:show), do: "Show Room"
+  defp page_title(:show, room_code), do: "Room: #{room_code}"
 end
