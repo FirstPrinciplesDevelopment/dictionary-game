976513099fa74a8825ee0210a8360d7f06c4b6c9
mellkior <38818928+mellkior@users.noreply.github.com>
2022-04-15 19:20:13 -0400

    improved UI and style

--- lib/dictionary_game_web/live/live_helpers.ex
+++ lib/dictionary_game_web/live/live_helpers.ex
@@ -27,10 +27,10 @@ defmodule DictionaryGameWeb.LiveHelpers do
     assigns = assign_new(assigns, :return_to, fn -> nil end)
 
     ~H"""
-    <div id="modal" class="phx-modal fade-in" phx-remove={hide_modal()}>
+    <div id="modal" class="fade-in opacity-10 fixed left-0 top-0 min-w-full min-h-full overflow-auto bg-[rgba(0,0,0,0.4)] hidden" phx-remove={hide_modal()}>
       <div
         id="modal-content"
-        class="phx-modal-content fade-in-scale"
+        class="bg-white fade-in-scale m-64 max-w-md mx-auto rounded-lg shadow-lg"
         phx-click-away={JS.dispatch("click", to: "#close")}
         phx-window-keydown={JS.dispatch("click", to: "#close")}
         phx-key="escape"
@@ -39,11 +39,11 @@ defmodule DictionaryGameWeb.LiveHelpers do
           <%= live_patch "✖",
             to: @return_to,
             id: "close",
-            class: "phx-modal-close",
+            class: "float-right text-2xl font-bold text-gray-500 cursor-pointer hover:text-gray-900 m-2",
             phx_click: hide_modal()
           %>
         <% else %>
-         <a id="close" href="#" class="phx-modal-close" phx-click={hide_modal()}>✖</a>
+         <a id="close" href="#" class="float-right text-2xl font-bold text-gray-500 cursor-pointer hover:text-gray-900 m-2" phx-click={hide_modal()}>✖</a>
         <% end %>
 
         <%= render_slot(@inner_block) %>
@@ -52,9 +52,15 @@ defmodule DictionaryGameWeb.LiveHelpers do
     """
   end
 
-  defp hide_modal(js \\ %JS{}) do
+  def hide_modal(js \\ %JS{}) do
     js
     |> JS.hide(to: "#modal", transition: "fade-out")
     |> JS.hide(to: "#modal-content", transition: "fade-out-scale")
+  end
+
+  def show_modal(js \\ %JS{}) do
+    js
+    |> JS.show(to: "#modal-content")
+    |> JS.show(to: "#modal")
   end
 end
